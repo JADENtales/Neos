eeb662d673d3e70917d5edb32fbfcb84b54de713
JADENtales <167289972+JADENtales@users.noreply.github.com>
2024-04-17 17:49:28 +0900

    add layout

--- src/lib.rs
+++ src/lib.rs
@@ -17,8 +17,9 @@ mod tui;
 
 #[derive(Debug, Default)]
 pub struct App {
-    counter: u8,
+    counter: i64,
     exit: bool,
+    vertical: bool,
 }
 
 impl App {
@@ -40,7 +41,31 @@ impl App {
     }
 
     fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
+        let layout = if self.vertical {
+            Layout::default().direction(Direction::Vertical).constraints(vec![
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+            ]).split(frame.size())
+        } else {
+            Layout::default().direction(Direction::Horizontal).constraints(vec![
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+                Constraint::Percentage(16),
+            ]).split(frame.size())
+        };
+        frame.render_widget(Paragraph::new("").block(Block::new().title(Title::from("全体")).borders(Borders::ALL)), layout[0]);
+        frame.render_widget(Paragraph::new("").block(Block::new().title(Title::from("一般")).borders(Borders::ALL)), layout[1]);
+        frame.render_widget(Paragraph::new("").block(Block::new().title(Title::from("耳打ち")).borders(Borders::ALL)), layout[2]);
+        frame.render_widget(Paragraph::new("").block(Block::new().title(Title::from("チーム")).borders(Borders::ALL)), layout[3]);
+        frame.render_widget(Paragraph::new("").block(Block::new().title(Title::from("クラブ")).borders(Borders::ALL)), layout[4]);
+        frame.render_widget(Paragraph::new("").block(Block::new().title(Title::from("システム")).borders(Borders::ALL)), layout[5]);
     }
 
     /// updates the application's state based on user input
@@ -78,76 +103,13 @@ impl App {
 
     fn increment_counter(&mut self) -> Result<()> {
         self.counter += 1;
-        if self.counter > 2 {
-            bail!("counter overflow");
-        }
         Ok(())
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" Counter App Tutorial ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]));
-        let block = Block::default()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .borders(Borders::ALL)
-            .border_set(border::THICK);
-
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
-
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn render() {
-        let app = App::default();
-        let mut buf = Buffer::empty(Rect::new(0, 0, 50, 4));
-
-        app.render(buf.area, &mut buf);
-
-        let mut expected = Buffer::with_lines(vec![
-            "┏━━━━━━━━━━━━━ Counter App Tutorial ━━━━━━━━━━━━━┓",
-            "┃                    Value: 0                    ┃",
-            "┃                                                ┃",
-            "┗━ Decrement <Left> Increment <Right> Quit <Q> ━━┛",
-        ]);
-        let title_style = Style::new().bold();
-        let counter_style = Style::new().yellow();
-        let key_style = Style::new().blue().bold();
-        expected.set_style(Rect::new(14, 0, 22, 1), title_style);
-        expected.set_style(Rect::new(28, 1, 1, 1), counter_style);
-        expected.set_style(Rect::new(13, 3, 6, 1), key_style);
-        expected.set_style(Rect::new(30, 3, 7, 1), key_style);
-        expected.set_style(Rect::new(43, 3, 4, 1), key_style);
-
-        // note ratatui also has an assert_buffer_eq! macro that can be used to
-        // compare buffers and display the differences in a more readable way
-        assert_eq!(buf, expected);
-    }
 
     #[test]
     fn handle_key_event() {
